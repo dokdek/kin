f870c2bb21613c75f8bc0c9ba806ba7b8c19c9f3
dokdek <dokdek@gmail.com>
2020-12-29 22:23:40 +0000

    Updated to work with new category model

--- src/components/helpers/renderCategorySelectGroup.js
+++ src/components/helpers/renderCategorySelectGroup.js
@@ -1,15 +1,14 @@
 
 
-import {MenuItem, ListSubheader} from '@material-ui/core'
 import React from 'react';
 
 export default function renderCategorySelectGroup(item, Main, Sub) {
     const items = item.subCategories.map((value, index) => {
       return (
-        <Sub key={index} value={value}>
-          {value}
+        <Sub key={index} value={value.name}>
+          {value.name}
         </Sub>
       );
     });
-    return [<Main>{item.category.name}</Main>, items];
+    return [<Main>{item.category}</Main>, items];
   }
