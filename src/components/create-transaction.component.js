306bbf1c71b764f90f5cccb4a4e5fdebf019413e
dokdek <dokdek@gmail.com>
2020-12-30 17:00:30 +0000

    Added form validation

--- src/components/create-transaction.component.js
+++ src/components/create-transaction.component.js
@@ -13,7 +13,6 @@ import {
 import Axios from "axios";
 import DateFnsUtils from "@date-io/date-fns";
 import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
-import { Redirect } from "react-router-dom";
 import getLists from "./helpers/getLists";
 import renderCategorySelectGroup from "./helpers/renderCategorySelectGroup";
 import renderPaymentSelectGroup from "./helpers/renderPaymentSelectGroup";
@@ -30,20 +29,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const CreateTransaction = ({ isAuth, username, open, setOpen }) => {
+const CreateTransaction = ({ username, open, setOpen }) => {
   const [description, setDescription] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState();
   const [date, setDate] = useState(new Date());
   const [categoryList, setCategoryList] = useState([]);
   const [category, setCategory] = useState("");
   const [paymentList, setPaymentList] = useState([]);
   const [payment, setPayment] = useState("");
+  const [formValidated, setFormValidated] = useState(true);
 
   const classes = useStyles();
 
+  function formCheck(){
+    console.log(description);
+    console.log(amount);
+    console.log(payment);
+    console.log(category);
+    if((description.length > 0) && amount && (payment.length > 0) && (category.length > 0)){
+      setFormValidated(false);
+    }
+  }
+
   useEffect(() => {
+    if(categoryList.length === 0){
     getLists(username, setCategoryList, setPaymentList);
-  }, []);
+    }
+    formCheck()
+  }, [description,amount,payment,category]);
 
   function onSubmit(e) {
     e.preventDefault();
@@ -75,76 +88,84 @@ const CreateTransaction = ({ isAuth, username, open, setOpen }) => {
   const handleClose = () => {
     setOpen(false);
   };
-    // const classes = useStyles();
-    return (
-      <Modal open={open} onClose={handleClose}>
-        <div style={{
-    top: `50%`,
-    left: `50%`,
-    transform: `translate(-50%, -50%)`,
-        }}className={classes.paper}>
-          <form noValidate autoComplete="off" onSubmit={onSubmit}>
-            <TextField
-              id="standard-basic"
-              label="Description"
-              onChange={(e) => {
-                setDescription(e.target.value);
+  // const classes = useStyles();
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <div
+        style={{
+          top: `50%`,
+          left: `50%`,
+          transform: `translate(-50%, -50%)`,
+        }}
+        className={classes.paper}
+      >
+        <form noValidate autoComplete="off" onSubmit={onSubmit}>
+          <FormControl required={true}>
+          <TextField
+            id="standard-basic"
+            label="Description"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+          </FormControl>
+          <TextField
+            id="filled-basic"
+            label="Amount"
+            onChange={(e) => {
+              setAmount(e.target.value)
+            }}
+            required={true}
+          />
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker
+              label="Date: "
+              value={date}
+              autoOk={true}
+              onChange={(e, date) => {
+                setDate(e);
               }}
             />
-            <TextField
-              id="filled-basic"
-              label="Amount"
+          </MuiPickersUtilsProvider>
+          <FormControl>
+            <InputLabel id="category-select">Category</InputLabel>
+            <Select
+              labelId="category-select"
+              value={category}
               onChange={(e) => {
-                setAmount(e.target.value);
+                setCategory(e.target.value);
               }}
-            />
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <DatePicker
-                label="Date: "
-                value={date}
-                autoOk={true}
-                onChange={(e, date) => setDate(e)}
-              />
-            </MuiPickersUtilsProvider>
-            <FormControl>
-              <InputLabel id="category-select">Category</InputLabel>
-              <Select
-                labelId="category-select"
-                value={category}
-                onChange={(e) => {
-                  setCategory(e.target.value);
-                }}
-                id="category-select"
-              >
-                {categoryList.map((cat) =>
-                  renderCategorySelectGroup(cat, ListSubheader, MenuItem)
-                )}{" "}
-                {/*Passes each cat through the render function, read above*/}
-              </Select>
-            </FormControl>
-            <FormControl>
-              <InputLabel id="payment-select">Account</InputLabel>
-              <Select
-                labelId="payment-select"
-                value={payment}
-                onChange={(e) => {
-                  setPayment(e.target.value);
-                }}
-                id="payment-select"
-              >
-                {paymentList.map((payment) =>
-                  renderPaymentSelectGroup(payment, ListSubheader, MenuItem)
-                )}{" "}
-                {/*Passes each pmt through the render function, read above*/}
-              </Select>
-            </FormControl>
-            <Button color="primary" variant="contained" type="submit">
-              Add
-            </Button>
-          </form>
-        </div>
-      </Modal>
-    );
+              id="category-select"
+            >
+              {categoryList.map((cat) =>
+                renderCategorySelectGroup(cat, ListSubheader, MenuItem)
+              )}{" "}
+              {/*Passes each cat through the render function, read above*/}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <InputLabel id="payment-select">Account</InputLabel>
+            <Select
+              labelId="payment-select"
+              value={payment}
+              onChange={(e) => {
+                setPayment(e.target.value);
+              }}
+              id="payment-select"
+            >
+              {paymentList.map((payment) =>
+                renderPaymentSelectGroup(payment, ListSubheader, MenuItem)
+              )}{" "}
+              {/*Passes each pmt through the render function, read above*/}
+            </Select>
+          </FormControl>
+          <Button color="primary" variant="contained" type="submit" disabled={formValidated}>
+            Add
+          </Button>
+        </form>
+      </div>
+    </Modal>
+  );
 };
 
 export default CreateTransaction;
